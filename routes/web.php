061fa2fe2ab469e6ba70d2f64270e2d86af253e7
son-12345 <142959430+son-12345@users.noreply.github.com>
2024-01-26 09:21:07 +0700

    555

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\ProductController;
 
+
+
+Route::resource('products', ProductController::class);
 Route::get('/', function () {
     return view('welcome');
 });
